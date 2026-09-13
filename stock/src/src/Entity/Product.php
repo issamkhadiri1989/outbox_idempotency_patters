fb055de6e0efeb76a8614c55b73dcc308b1885f1
Issam KHADIRI <khadiri.issam@gmail.com>
2026-09-13 12:35:05 +0100

    add Product operations

--- stock/src/src/Entity/Product.php
+++ stock/src/src/Entity/Product.php
@@ -2,12 +2,16 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Metadata\GetCollection;
+use ApiPlatform\Metadata\Get;
 use App\Repository\ProductRepository;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Bridge\Doctrine\Types\UuidType;
 use Symfony\Component\Uid\Uuid;
 
 #[ORM\Entity(repositoryClass: ProductRepository::class)]
+#[GetCollection()]
+#[Get]
 class Product
 {
     #[ORM\Id]
